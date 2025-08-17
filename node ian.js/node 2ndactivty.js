461eda8ebdcfca6aadfbbe26252709a53ff98626
benzianmanuta-ui <benzian.manuta@hcdc.edu.ph>
2025-08-17 13:52:25 +0800

    Add files via upload

---
+++ node ian.js/node 2ndactivty.js
@@ -0,0 +1,29 @@
+var myName = "Sherwin Awa"
+console.log("Name: " ,myName);
+
+let age = "20";
+console.log(age);
+
+const PI = 3.1416;
+console.log(PI);
+
+let isStudent = Number(age) < 25;
+console.log(isStudent);
+
+let person = {
+    firstName: "Sherwin",
+
+    middleinitial: "flores",
+    lastname:  "Awa",
+address: {
+    barrangay: "Panacan ",
+    city: "Davao City",
+    province: "Davao del Sur"
+ }
+};
+
+console.log(person.firstName, person.middleinitial, person.lastname);
+console.log(person.address.barrangay, person.address.city, person.address.province);
+
+let colors = ["Blue", "violet", "purple", "yellow", "red"]
+console.log(colors);
